60f67d948d41a2489940f4ece72ab18f50e2b414
Aji0One <ajithkumarmspva@gmail.com>
2024-01-28 14:32:24 +0530

    done fe corrections

--- src/Components/Resume/resume.js
+++ src/Components/Resume/resume.js
@@ -10,7 +10,7 @@ const Resume = () => {
       <a
         id="resume"
         target="_blank"
-        href="https://drive.google.com/file/d/1L_ns2RRtdB1gLzz1KzbvW_vk4h_n36fD/view?google_abuse=GOOGLE_ABUSE_EXEMPTION%3DID%3D56a4f3817bb827c7:TM%3D1706432431:C%3Dr:IP%3D157.51.160.104-:S%3DzUzCL2FmXcrAyiBS-G29iSk%3B+path%3D/%3B+domain%3Dgoogle.com%3B+expires%3DSun,+28-Jan-2024+12:00:31+GMT"
+        href="https://drive.google.com/file/d/1L_ns2RRtdB1gLzz1KzbvW_vk4h_n36fD/view"
         className="btn"
         onMouseOver={handleBtn}
       >
